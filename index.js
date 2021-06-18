60bfe6c73dd9954b8fbf5283c1c2f306e8a38d42
Salil Deogade <salil.deogade@gmail.com>
2021-06-18 15:43:45 +0530

    Added information for our codes and now only 10 random questions gets generated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const quizDB = [
+const allQuizQuestions = [
     {
-        question: "Q1: What is the full form of HTML?",
+        question: "What is the full form of HTML?",
         a: "Hello To My Land",
         b: "Hey Text Markup Language",
         c: "Hypes Making Language",
@@ -9,7 +9,7 @@ const quizDB = [
     },
 
     {
-        question: "Q2: What is the full form of JS?",
+        question: "What is the full form of JS?",
         a: "JavaScript",
         b: "Hey Text Markup Language",
         c: "HyperText Markup Language",
@@ -18,7 +18,7 @@ const quizDB = [
     },
 
     {
-        question: "Q3: What is the full form of SD?",
+        question: "What is the full form of SD?",
         a: "Hello To My Land",
         b: "Hey Text Markup Language",
         c: "Salil Deogade",
@@ -26,15 +26,153 @@ const quizDB = [
         ans: "ans3"
     },
     {
-        question: "Q4: What is the full form of OS?",
+        question: "What is the full form of fs?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Salil Deogade",
+        d: "HyperText Markup Language",
+        ans: "ans3"
+    },
+    {
+        question: "What is the full form of 324?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Salil Deogade",
+        d: "HyperText Markup Language",
+        ans: "ans3"
+    },
+    {
+        question: "What is the full form of S532?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Salil Deogade",
+        d: "HyperText Markup Language",
+        ans: "ans3"
+    },
+    {
+        question: "What is the full form of 675?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Salil Deogade",
+        d: "HyperText Markup Language",
+        ans: "ans3"
+    },
+    {
+        question: "What is the full form of 123?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Salil Deogade",
+        d: "HyperText Markup Language",
+        ans: "ans3"
+    },
+    {
+        question: "What is the full form of 1234?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Salil Deogade",
+        d: "HyperText Markup Language",
+        ans: "ans3"
+    },
+    {
+        question: "What is the full form of 4564?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Salil Deogade",
+        d: "HyperText Markup Language",
+        ans: "ans3"
+    },
+    {
+        question: "What is the full form of gdff?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Salil Deogade",
+        d: "HyperText Markup Language",
+        ans: "ans3"
+    },
+    {
+        question: "What is the full form of lol?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Salil Deogade",
+        d: "HyperText Markup Language",
+        ans: "ans3"
+    },
+    {
+        question: "What is the full form of vall?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Salil Deogade",
+        d: "HyperText Markup Language",
+        ans: "ans3"
+    },
+    {
+        question: "What is the full form of gajj?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Salil Deogade",
+        d: "HyperText Markup Language",
+        ans: "ans3"
+    },
+    {
+        question: "What is the full form of niib?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Salil Deogade",
+        d: "HyperText Markup Language",
+        ans: "ans3"
+    },
+    {
+        question: "What is the full form of cat?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Salil Deogade",
+        d: "HyperText Markup Language",
+        ans: "ans3"
+    },
+    {
+        question: "What is the full form of red?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Salil Deogade",
+        d: "HyperText Markup Language",
+        ans: "ans3"
+    },
+    {
+        question: "What is the full form of 34?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Salil Deogade",
+        d: "HyperText Markup Language",
+        ans: "ans3"
+    },
+    {
+        question: "What is the full form of 2sda?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Salil Deogade",
+        d: "HyperText Markup Language",
+        ans: "ans3"
+    },
+    {
+        question: "What is the full form of te?",
         a: "Hello To My Land",
         b: "Operating Systems",
         c: "HyperText Markup Language",
         d: "HyperText Markup Language",
         ans: "ans2"
     }
+]
 
-];
+// Shuffles our all questions array every time the page is loaded
+allQuizQuestions.sort(() => 0.5 - Math.random())
+
+// Makes set of 10 random questions from our all questions array
+const quizDB = [];
+for (let i = 0; i < 10; i++) {
+    quizDB.push(allQuizQuestions[i])
+}
+
+
 const question = document.querySelector('.question');
 const option1 = document.querySelector('#option1');
 const option2 = document.querySelector('#option2');
@@ -53,7 +191,7 @@ let questionCount = 0;
 
 const loadQuestion = () => {
 
-    const questionList = quizDB[questionCount];
+    const questionList = quizDB[questionCount];  
 
     question.innerText = questionList.question;
 
@@ -66,6 +204,7 @@ const loadQuestion = () => {
 loadQuestion();
 
 
+// This function detects the option which the user has selected and stores in the variable "userMarkedOption"
 let attempted = false;
 const markedOption = () => {
 
@@ -77,65 +216,75 @@ const markedOption = () => {
             attempted = true;
         }
 
-
-
     });
 
     return userMarkedOption;
 
 };
 
+// This function removes the user response of the question which was selected from the previous question 
 const deselectAll = () => {
     answers.forEach((curAnsElement) => curAnsElement.checked = false); //
 }
 
+// This function triggers when the user click on submit without selecting any option
 let alertDiv = document.querySelector("#alert-div")
 const showAlert = () => {
     alertDiv.style.visibility = "visible"
 }
 
+// This div shows the status of the questions and it gets updated every time the user jumps to the next question
 statusDiv.innerHTML = `Question ${questionCount + 1} of ${quizDB.length}`
 
 let score = 0;
 submit.addEventListener('click', () => {
-    const checkedAnswer = markedOption();
+    const checkedAnswer = markedOption();  // the value that markedOption() returned is stored in this variable
     console.log(checkedAnswer);
     console.log(attempted)
+
+    // clicked on submit if no option is selected
     if (!attempted) {
         showAlert();
     }
+    // clicked on submit after selecting a option
     else {
         statusDiv.innerHTML = `Question ${questionCount + 2} of ${quizDB.length}`
         alertDiv.style.visibility =  'hidden'
         attempted = false
+
+        // this updates the score
         if (checkedAnswer == quizDB[questionCount].ans) {
             score++;
             scoreCount.innerText = score
         };
 
-        questionCount++;
+        questionCount++;  // moves to the next question
 
-        deselectAll();
+        deselectAll();  // clears the response from the previous question
 
         if (questionCount < quizDB.length) {
             loadQuestion();
-        } else {
+        } 
+        
+        // at the end of the quiz below code will work OR it is the result section
+        else {
+            // If every answer is correct
             if (score == quizDB.length) {
                 showScore.innerHTML = `
     
                  <h3> <b>Congratulations!!<b> You scored 100%</h3>
-                 <button class="btn" onclick="location.reload()">Play Again</button> `;
+                 <button class="btn" onclick="location.reload()">Play Again</button> `;  // this button reloads the page and thereby quiz gets reset and starts from the beginning.
 
             } else {
                 showScore.innerHTML = `
     
                  <h3> You scored ${score}/${quizDB.length}</h3>
-                 <button class="btn" onclick="location.reload()">Play Again</button> `;
+                 <button class="btn" onclick="location.reload()">Play Again</button> `;  // this button reloads the page and thereby quiz gets reset and starts from the beginning.
 
             }
 
 
-
+            // this section removes the elements like questions,answers and shows only required elements on the result page
             showScore.classList.remove('scoreArea');
             submit.style.display = "none"
             contentDiv.style.display = "none"
