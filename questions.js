094538153eadf3377bb47d5c448e37151eba2dc8
Salil Deogade <salil.deogade@gmail.com>
2021-07-31 19:35:10 +0530

    Imported Questions from questions.js

---
+++ questions.js
@@ -0,0 +1,164 @@
+export const allQuizQuestions = [
+    {
+        question: "What is the full form of HTML?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Hypes Making Language",
+        d: "HyperText Markup Language",
+        ans: "ans4"
+    },
+
+    {
+        question: "What is the full form of JS?",
+        a: "JavaScript",
+        b: "Hey Text Markup Language",
+        c: "HyperText Markup Language",
+        d: "HyperText Markup Language",
+        ans: "ans1"
+    },
+
+    {
+        question: "What is the full form of SD?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Salil Deogade",
+        d: "HyperText Markup Language",
+        ans: "ans3"
+    },
+    {
+        question: "What is the full form of fs?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Salil Deogade",
+        d: "HyperText Markup Language",
+        ans: "ans3"
+    },
+    {
+        question: "What is the full form of 324?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Salil Deogade",
+        d: "HyperText Markup Language",
+        ans: "ans3"
+    },
+    {
+        question: "What is the full form of S532?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Salil Deogade",
+        d: "HyperText Markup Language",
+        ans: "ans3"
+    },
+    {
+        question: "What is the full form of 675?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Salil Deogade",
+        d: "HyperText Markup Language",
+        ans: "ans3"
+    },
+    {
+        question: "What is the full form of 123?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Salil Deogade",
+        d: "HyperText Markup Language",
+        ans: "ans3"
+    },
+    {
+        question: "What is the full form of 1234?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Salil Deogade",
+        d: "HyperText Markup Language",
+        ans: "ans3"
+    },
+    {
+        question: "What is the full form of 4564?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Salil Deogade",
+        d: "HyperText Markup Language",
+        ans: "ans3"
+    },
+    {
+        question: "What is the full form of gdff?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Salil Deogade",
+        d: "HyperText Markup Language",
+        ans: "ans3"
+    },
+    {
+        question: "What is the full form of lol?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Salil Deogade",
+        d: "HyperText Markup Language",
+        ans: "ans3"
+    },
+    {
+        question: "What is the full form of vall?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Salil Deogade",
+        d: "HyperText Markup Language",
+        ans: "ans3"
+    },
+    {
+        question: "What is the full form of gajj?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Salil Deogade",
+        d: "HyperText Markup Language",
+        ans: "ans3"
+    },
+    {
+        question: "What is the full form of niib?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Salil Deogade",
+        d: "HyperText Markup Language",
+        ans: "ans3"
+    },
+    {
+        question: "What is the full form of cat?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Salil Deogade",
+        d: "HyperText Markup Language",
+        ans: "ans3"
+    },
+    {
+        question: "What is the full form of red?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Salil Deogade",
+        d: "HyperText Markup Language",
+        ans: "ans3"
+    },
+    {
+        question: "What is the full form of 34?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Salil Deogade",
+        d: "HyperText Markup Language",
+        ans: "ans3"
+    },
+    {
+        question: "What is the full form of 2sda?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Salil Deogade",
+        d: "HyperText Markup Language",
+        ans: "ans3"
+    },
+    {
+        question: "What is the full form of te?",
+        a: "Hello To My Land",
+        b: "Operating Systems",
+        c: "HyperText Markup Language",
+        d: "HyperText Markup Language",
+        ans: "ans2"
+    }
+]
